b28e614eb8ecfaa87436ecd598b5414639d26847
can <canavci2016@gmail.com>
2022-01-15 13:22:20 +0300

    database loader was optimized to load all sort of response

--- src/Loaders/DataBase.js
+++ src/Loaders/DataBase.js
@@ -1,16 +1,21 @@
-const sequelize = require('./Sequelize');
+const {connection, ...res} = require('./Sequelize');
 let connectionPools = {};
+console.log(res);
 
+let obj = {
+  pools: connectionPools,
+  addConnection: (key, conObj) => {
+    connectionPools[key] = connection({
+      host: conObj.host,
+      database: conObj.database,
+      user: conObj.user,
+      password: conObj.password,
+      driver: conObj.driver ? conObj.driver : "mssql"
+    });
+  },
+  ...res
 
-module.exports.pools = connectionPools;
-module.exports.addConnection = (key, conObj) => {
-  connectionPools[key] = sequelize.connection({
-    host: conObj.host,
-    database: conObj.database,
-    user: conObj.user,
-    password: conObj.password,
-    driver: conObj.driver ? conObj.driver : "mssql"
-  });
 };
 
 
+module.exports = obj;
