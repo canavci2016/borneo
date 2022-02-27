4fad44938e5cce75c41c606bfdaa277dba49f92f
can <canavci2016@gmail.com>
2022-02-27 10:42:56 +0000

    events were added for mongo driver

--- src/Loaders/Mongoose.js
+++ src/Loaders/Mongoose.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 module.exports.connection = ({host, ...opt}) => {
   let {driver, connector, ...options} = opt;
-  return mongoose.createConnection(host, options);
+  let db = mongoose.createConnection(host, options);
+
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log(host + "Connected successfully");
+  });
+
+  return db;
 };
 
 
